ccf1ab172209a8b53caa7a404a34723cae1fc0fc
kiyomasa84 <bret.k.ikehara@gmail.com>
2013-01-27 00:02:26 -1000

    First commit of robocode.

---
+++ src/edu/hawaii/breti/robocode/TousouchuuHunter.java
@@ -0,0 +1,20 @@
+package edu.hawaii.breti.robocode;
+
+import robocode.Robot;
+
+/**
+ * Provides the implementation for a Robocode {@link Robot}
+ * 
+ * @author Bret K. Ikehara
+ * 
+ */
+public class TousouchuuHunter extends Robot {
+
+  @Override
+  public void run() {
+
+    while (true) {
+
+    }
+  }
+}
