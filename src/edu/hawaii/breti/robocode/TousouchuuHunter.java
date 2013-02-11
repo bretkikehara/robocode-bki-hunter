856848a4c09a1ea665b7fe40da4f195eed7c2d3b
bikehara <bret.k.ikehara@gmail.com>
2013-02-10 21:00:58 -1000

    Changed to advanced robot. Removed private instance of scanned event from robot.

--- src/edu/hawaii/breti/robocode/TousouchuuHunter.java
+++ src/edu/hawaii/breti/robocode/TousouchuuHunter.java
@@ -1,20 +1,43 @@
 package edu.hawaii.breti.robocode;
 
-import robocode.Robot;
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
 
 /**
- * Provides the implementation for a Robocode {@link Robot}
+ * Provides the implementation for a Robocode {@link AdvancedRobot}
  * 
  * @author Bret K. Ikehara
  * 
  */
-public class TousouchuuHunter extends Robot {
+public class TousouchuuHunter extends AdvancedRobot {
 
+  private ScannedRobotEvent event;
+  
   @Override
   public void run() {
-
+    this.turnRadarRight(360);
     while (true) {
+      // find all enemies.
+      this.setTurnRadarRight(360);
 
+      if (this.event != null) {
+        this.setAhead(100);
+        double turnAngle = this.event.getHeading() - this.getHeading();
+        turnAngle = RobotHelper.calculateOptimalAngle(turnAngle);
+        this.setTurnRight(turnAngle);
+        this.event = null;
+      }
+      this.execute();
+    }
+  }
+
+  @Override
+  public void onScannedRobot(ScannedRobotEvent event) {
+    if (this.event == null) {
+      this.event = event;
+    }
+    else if (this.event.equals(event.getName()) || event.getDistance() < this.event.getDistance()) {
+      this.event = event;
     }
   }
 }
