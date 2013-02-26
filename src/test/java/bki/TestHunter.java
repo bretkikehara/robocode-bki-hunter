5aa1e4c92f8bc97ddc0c657862895ecd5ff73d9f
kiyomasa84 <bret.k.ikehara@gmail.com>
2013-02-25 19:27:40 -1000

    Added a fire power test.

--- src/test/java/bki/TestHunter.java
+++ src/test/java/bki/TestHunter.java
@@ -1,6 +1,11 @@
 package bki;
 
+import static org.junit.Assert.assertTrue;
+import robocode.BattleResults;
+import robocode.Rules;
+import robocode.control.events.BattleCompletedEvent;
 import robocode.control.events.TurnEndedEvent;
+import robocode.control.snapshot.IBulletSnapshot;
 import robocode.control.snapshot.IRobotSnapshot;
 import robocode.control.snapshot.ITurnSnapshot;
 import robocode.control.testing.RobotTestBed;
@@ -11,6 +16,8 @@ import robocode.control.testing.RobotTestBed;
  * @author Bret K. Ikehara
  */
 public class TestHunter extends RobotTestBed {
+
+  final String myRobotName = "bki.Hunter*";
 
   /**
    * Specifies that SittingDuck and DaCruzer are to be matched up in this test case.
@@ -32,7 +39,17 @@ public class TestHunter extends RobotTestBed {
     return 20;
   }
 
-  /**
+  @Override
+  public void onBattleCompleted(BattleCompletedEvent event) {
+    for (BattleResults results : event.getIndexedResults()) {
+      if (myRobotName.equals(results.getTeamLeaderName())) {
+        boolean check = results.getFirsts() > 10;
+        assertTrue("Won at least 50%", check);
+      }
+    }
+  }
+
+  /*
    * Tests what happens on turn end.
    * 
    * @param event {@link TurnEndedEvent}
@@ -40,14 +57,34 @@ public class TestHunter extends RobotTestBed {
   @Override
   public void onTurnEnded(TurnEndedEvent event) {
     ITurnSnapshot turn = event.getTurnSnapshot();
+    IRobotSnapshot[] robots = turn.getRobots();
 
-    for (IRobotSnapshot robot : turn.getRobots()) {
-      if ("bki".equals(robot.getName())) {
-        // TODO fix test.
-        // if (robot.getX())
-        // robot.getBodyHeading()
+    // tests the firepower.
+    for (IBulletSnapshot bullet : turn.getBullets()) {
+      IRobotSnapshot robot = robots[bullet.getOwnerIndex()];
+      if (myRobotName.equals(robot.getName())) {
+        boolean test = testDouble(Rules.MAX_BULLET_POWER, bullet.getPower(), 0.3);
+        if (test) {
+          assertTrue("Always max fire power: " + bullet.getPower(), test);
+        }
+        else {
+          // previous bullet may have hit gaining us health, so health can be as much as 10.
+          test = robot.getEnergy() < 10.0;
+          assertTrue("Low life: " + robot.getEnergy(), test);
+        }
       }
     }
   }
 
+  /**
+   * Checks whether a double is equals with offset.
+   * 
+   * @param expected double
+   * @param value double
+   * @param epsilon double
+   * @return boolean
+   */
+  public boolean testDouble(double expected, double value, double epsilon) {
+    return (expected - epsilon < value) && (expected + epsilon > value);
+  }
 }
